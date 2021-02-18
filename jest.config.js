6f8c2ad143d91f7f648b57a8433f515ff03e9715
sharmin <sharminh@sina.cn>
2021-02-18 14:47:06 +0800

    chore: 引入jest-transform-stub

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,8 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
     '^.+\\.jsx?$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '.*\\.(vue)$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub'
   },
   collectCoverage: false,
   collectCoverageFrom: [
